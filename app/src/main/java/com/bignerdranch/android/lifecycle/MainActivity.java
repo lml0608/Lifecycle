512deeadeb63c7ec3b098d2891cafea53965ab8f
liubin001 <183773928@qq.com>
2017-04-28 08:48:40 +0800

    savedInstanceState

--- app/src/main/java/com/bignerdranch/android/lifecycle/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/lifecycle/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.lifecycle;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String ON_RESTART = "onRestart";
     private static final String ON_DESTROY = "onDestroy";
     private static final String ON_SAVE_INSTANCE_STATE = "onSaveInstanceState";
+    private static final String LIFECYCLE_CALLBACKS_TEXT_KEY = "callbacks";
 
     /*
      * This TextView will contain a running log of every lifecycle callback method called from this
@@ -48,7 +50,24 @@ public class MainActivity extends AppCompatActivity {
         mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
 
         // COMPLETED (1) Use logAndAppend within onCreate
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(LIFECYCLE_CALLBACKS_TEXT_KEY)) {
+                String allPreviousLifecycleCallbacks = savedInstanceState.getString(LIFECYCLE_CALLBACKS_TEXT_KEY);
+                mLifecycleDisplay.setText(allPreviousLifecycleCallbacks);
+            }
+        }
+
         logAndAppend(ON_CREATE);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        logAndAppend(ON_SAVE_INSTANCE_STATE);
+        String lifecycleDisplayTextViewContents =
+                mLifecycleDisplay.getText().toString();
+        outState.putString(LIFECYCLE_CALLBACKS_TEXT_KEY, lifecycleDisplayTextViewContents);
     }
 
     // COMPLETED (2) Override onStart, call super.onStart, and call logAndAppend with ON_START
